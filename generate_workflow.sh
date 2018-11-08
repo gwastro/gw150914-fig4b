8fae312aa924fb89005cd376b30594603574dc42
Duncan Brown <dabrown@syr.edu>
2018-11-08 08:11:45 -0500

    run pycbc_losc_segment_query from a wrapper to protect its environment

--- generate_workflow.sh
+++ generate_workflow.sh
@@ -1,13 +1,38 @@
 #!/bin/bash -v
 
+git clean -dxf
+
+virtualenv pycbc-v1.13.0
+source pycbc-v1.13.0/bin/activate
+pip install --upgrade pip setuptools
+pip install -r https://raw.githubusercontent.com/gwastro/pycbc/v1.13.0/requirements.txt
+pip install 'pycbc==1.13.0'
+pip install 'lalsuite==6.48.1.dev20180717'
+deactivate
+export PYCBC_1_13_0_PATH=${PWD}/pycbc-v1.13.0
+
+cat > ${PYCBC_1_13_0_PATH}/bin/pycbc_losc_segment_query.sh << EOF
+#!/bin/bash
+
+unset LD_LIBRARY_PATH
+unset PYTHONPATH
+
+source ${PYCBC_1_13_0_PATH}/bin/activate
+
+echo "Calling ${PYCBC_1_13_0_PATH}/bin/pycbc_losc_segment_query \${@}"
+
+${PYCBC_1_13_0_PATH}/bin/pycbc_losc_segment_query \${@}
+
+exit \$?
+EOF
+chmod +x ${PYCBC_1_13_0_PATH}/bin/pycbc_losc_segment_query.sh
+
 wget https://git.ligo.org/ligo-cbc/pycbc-software/raw/master/v1.3.2/x86_64/composer_xe_2015.0.090/pycbc_make_coinc_search_workflow
 wget https://git.ligo.org/ligo-cbc/pycbc-software/raw/master/v1.3.2/x86_64/composer_xe_2015.0.090/pycbc_submit_dax
 chmod +x pycbc_make_coinc_search_workflow pycbc_submit_dax
 
 wget https://git.ligo.org/ligo-cbc/pycbc-software/raw/efd37637fbb568936dfb92bc7aa8a77359c9aa36/v1.3.2/x86_64/composer_xe_2015.0.090/executables.ini
 perl -pi.bak -e 's+http://code.pycbc.phy.syr.edu/pycbc-software/+https://git.ligo.org/ligo-cbc/pycbc-software/raw/master/+g' executables.ini
-
-source /cvmfs/oasis.opensciencegrid.org/ligo/sw/pycbc/x86_64_rhel_7/virtualenv/pycbc-v1.13.0/bin/activate
 
 WORKFLOW_NAME=gw150914-16day-c01-v1.3.2
 OUTPUT_PATH=${HOME}/secure_html/gw150914/${WORKFLOW_NAME}
@@ -29,7 +54,7 @@ OUTPUT_PATH=${HOME}/secure_html/gw150914/${WORKFLOW_NAME}
       "coinc-full:loudest-keep:5000" \
       "pegasus_profile-distribute_background_bins:condor|request_memory:200000" \
       "pegasus_profile-statmap:condor|request_memory:200000" \
-      "executables:segment_query:/cvmfs/oasis.opensciencegrid.org/ligo/sw/pycbc/x86_64_rhel_7/virtualenv/pycbc-v1.13.0/bin/pycbc_losc_segment_query" \
+      "executables:segment_query:${PYCBC_1_13_0_PATH}/bin/pycbc_losc_segment_query.sh" \
       "workflow:h1-channel-name:H1:GWOSC-16KHZ_R1_STRAIN" \
       "workflow:l1-channel-name:L1:GWOSC-16KHZ_R1_STRAIN" \
       "workflow-datafind:datafind-h1-frame-type:H1_LOSC_16_V1" \
