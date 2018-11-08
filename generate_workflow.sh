1fd6563e4c7c3dec8724d56a02fb9bfae791ea3e
Duncan Brown <dabrown@syr.edu>
2018-11-07 19:54:02 -0500

    download executables file and set paths to git.ligo.org

--- generate_workflow.sh
+++ generate_workflow.sh
@@ -2,6 +2,10 @@ wget https://git.ligo.org/ligo-cbc/pycbc-software/raw/master/v1.3.2/x86_64/compo
 wget https://git.ligo.org/ligo-cbc/pycbc-software/raw/master/v1.3.2/x86_64/composer_xe_2015.0.090/pycbc_submit_dax
 chmod +x pycbc_make_coinc_search_workflow pycbc_submit_dax
 
+wget https://git.ligo.org/ligo-cbc/pycbc-software/raw/efd37637fbb568936dfb92bc7aa8a77359c9aa36/v1.3.2/x86_64/composer_xe_2015.0.090/executables.ini
+perl -pi.bak -e 's+http://code.pycbc.phy.syr.edu/pycbc-software/+https://git.ligo.org/ligo-cbc/pycbc-software/raw/master/+g'
+executables.ini
+
 WORKFLOW_NAME=gw150914-16day-c01-v1.3.2
 OUTPUT_PATH=${HOME}/secure_html/gw150914/${WORKFLOW_NAME}
 ./pycbc_make_coinc_search_workflow --workflow-name \
@@ -10,7 +14,7 @@ OUTPUT_PATH=${HOME}/secure_html/gw150914/${WORKFLOW_NAME}
       https://raw.githubusercontent.com/gwastro/pycbc-config/a227ac6d4accdf5d9b4e51b93cc92543fe7e1ebc/O1/pipeline/plotting.ini \
       https://raw.githubusercontent.com/gwastro/pycbc-config/541d4dc77d4d0fbc1b622ed0af4ef17f02fca685/ER8/pipeline/data_C01.ini \
       https://raw.githubusercontent.com/gwastro/pycbc-config/541d4dc77d4d0fbc1b622ed0af4ef17f02fca685/ER8/pipeline/gps_times_ER8B_analysis_16day_C01.ini \
-      https://git.ligo.org/ligo-cbc/pycbc-software/raw/master/v1.3.2/x86_64/composer_xe_2015.0.090/executables.ini \
+      executables.ini \
       --config-overrides \
       "results_page:output-path:${OUTPUT_PATH}" \
       "coinc-full:timeslide-interval:0.1" \
