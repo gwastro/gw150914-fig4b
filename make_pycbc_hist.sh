8b09e5b905a2c1cf0b221c8793ac86937d53dea5
Duncan Brown <dabrown@syr.edu>
2018-11-14 21:21:41 -0500

    use statmap file created by this workflow

--- make_pycbc_hist.sh
+++ make_pycbc_hist.sh
@@ -6,4 +6,4 @@ if [ ! -z ${VIRTUAL_ENV} ] ; then
 
 source /home/dbrown/src/pycbc-1.3.4/bin/activate
 
-./pycbc_dogsin_hist_sigmas_arrow --exclusive-bkg --trigger-file ./gw150914-16day-c01-v1.3.2/H1L1-STATMAP_FULL_DATA_FULL_CUMULATIVE_CAT_12H_FULL_DATA_FULL_BIN_1-1126051217-3331800.hdf --x-min 7.85 --x-max 25.2 --y-min 1e-8 --y-max 200 --trials-factor 3 --output-file pycbc-hist-0p1s-sigmas-arrow.pdf --bin-size 0.2
+./pycbc_dogsin_hist_sigmas_arrow --exclusive-bkg --trigger-file ./output/full_data/H1L1-STATMAP_FULL_DATA_FULL_CUMULATIVE_CAT_12H_FULL_DATA_FULL_BIN_1-1126051217-3331800.hdf --x-min 7.85 --x-max 25.2 --y-min 1e-8 --y-max 200 --trials-factor 3 --output-file pycbc-hist-0p1s-sigmas-arrow.pdf --bin-size 0.2
